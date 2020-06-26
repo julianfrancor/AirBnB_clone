5d29aa421054111d4aa8a1e8a5ca4461ab562169
julianfrancor <julianfrancor@gmail.com>
2020-06-26 02:48:03 +0000

    task 4 - Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,8 @@ BaseModel class that defines all common attributes/methods for other classes
 """
 import uuid
 from datetime import datetime
+isoform_time = "%Y-%m-%dT%H:%M:%S.%f"
+
 
 class BaseModel:
     """
@@ -16,17 +18,29 @@ class BaseModel:
             created_at -> Public instance attributes
             updated_at -> Public instance attributes
     """
+
     def __init__(self, *args, **kwargs):
         """
         Initialization of the object/instance attributes
+            id: contains the object's identification
+            created_at: the datetime in which the object was created
+            updated_at: the datetime in which the object was modified
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is None or len(kwargs) == 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, isoform_time)
 
     def __str__(self):
         """ Writing the __str__ method """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         """ Public instance methods:
@@ -39,9 +53,12 @@ class BaseModel:
         """ Public instance methods
             returns a dictionary containing all keys/values
             of __dict__ of the instance with self.__dict__
-            we are making a copy
+            we are making a copy.
+            This method will be the first piece of the
+            serialization/deserialization process: create a dictionary
+            representation with “simple object type” of our BaseModel
         """
-        dic_BaseClass = self.__dict__
+        dic_BaseClass = self.__dict__.copy()
         dic_BaseClass["__class__"] = self.__class__.__name__
         dic_BaseClass["created_at"] = self.created_at.isoformat()
         dic_BaseClass["updated_at"] = self.updated_at.isoformat()
