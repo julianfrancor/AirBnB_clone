56acdbb577c72684bf6e36a92e0cdda8f29420a3
PabloYepes27 <juanpabloyepest@gmail.com>
2020-06-27 01:45:22 +0000

    Task 5 to have a file storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+class FileStorage that serializes instances to a JSON file and deserializes
+JSON file to instances
+"""
+import os
+import json
+from models.base_model import BaseModel
+isoform_time = "%Y-%m-%dT%H:%M:%S.%f"
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def __init__(self):
+        """[summary]
+        """
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        json_dict = {}
+        for key, value in FileStorage.__objects.items():
+            json_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, mode="w", encoding="UTF8") as file:
+            json.dump(json_dict, file)
+
+    def reload(self):
+        """deserializes the JSON file to __objects (only if the JSON file
+        (__file_path) exists ; otherwise, do nothing. If the file doesn’t
+        exist, no exception should be raised)"""
+        try:
+            with open(FileStorage.__file_path, mode="r",
+                      encoding="UTF8") as file:
+                json_dict = json.load(file)
+            for key, value in json_dict.items():
+                FileStorage.__objects[key] = BaseModel(**value)
+        except Exception:
+            pass
