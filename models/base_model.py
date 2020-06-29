6fa1675164e7ebf0f0cd1a86f2153400b3039abb
PabloYepes27 <juanpabloyepest@gmail.com>
2020-06-29 22:56:21 +0000

    pep 8

--- models/base_model.py
+++ models/base_model.py
@@ -31,14 +31,13 @@ class BaseModel:
             self.updated_at = datetime.now()
             models.storage.new(self)
         else:
-            # self.__dict__ = kwargs  # this is a new update
             for key, value in kwargs.items():
                 if key == "id":
                     self.id = value
                 elif key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.strptime(value, isoform_time)
-                elif key is "__class__": # de aqui para abajo fue para arrelgar
-                    pass # el problema de intanciacion con el __class__
+                elif key is "__class__":
+                    pass
                 elif key is not "__class__":
                     self.__dict__[key] = value
 
