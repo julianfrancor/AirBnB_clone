b6496460a6bc6a86e5dc56d9dee833b4a3024397
julianfrancor <julianfrancor@gmail.com>
2020-06-30 04:35:52 +0000

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,9 +6,9 @@ JSON file to instances
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.city import City
 from models.place import Place
 from models.state import State
-from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 import os
