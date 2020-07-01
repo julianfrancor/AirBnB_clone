79c68a71fe53d63ce33d87241af41ab7b6390b7a
PabloYepes27 <juanpabloyepest@gmail.com>
2020-07-01 18:42:19 +0000

    Task 5 - checks

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -91,9 +91,66 @@ class TestFileStorage(unittest.TestCase):
             fail = "descriptor '__init__' of 'object' object needs an argument"
             self.assertEqual(str(error.exception), fail)
 
+    def test_all(self):
+        """ Test all witout any object"""        
+
     def test_arguments(self):
         """ tests __init__ with many arguments"""
         with self.assertRaises(TypeError) as error:
             base = FileStorage(7, 12)
         fail = "object() takes no parameters"
         self.assertEqual(str(error.exception), fail)
+
+    def test_all_dict_returned(self):
+        """test the method all when returns dict"""
+        file = FileStorage()
+        dicto = file.all()
+        self.assertIs(dicto, file._FileStorage__objects)
+        self.assertEqual(type(dicto), dict)
+
+    def test_new(self):
+        """test the method new at the creation of new object"""
+        file = FileStorage()
+        save = FileStorage._FileStorage__objects
+        FileStorage._FileStorage__objects = {}
+        test_dict = {}
+        for key, value in classes.items():
+            with self.subTest(key=key, value=value):
+                instance = value()
+                instance_key = instance.__class__.__name__ + "." + instance.id
+                file.new(instance)
+                test_dict[instance_key] = instance
+                self.assertEqual(test_dict, file._FileStorage__objects)
+        FileStorage._FileStorage__objects = save
+
+    def test_save(self):
+        """Test that save properly saves objects to file.json"""
+        file = FileStorage()
+        new_dict = {}
+        for key, value in classes.items():
+            instance = value()
+            instance_key = instance.__class__.__name__ + "." + instance.id
+            new_dict[instance_key] = instance
+        save = FileStorage._FileStorage__objects
+        FileStorage._FileStorage__objects = new_dict
+        file.save()
+        FileStorage._FileStorage__objects = save
+        for key, value in new_dict.items():
+            new_dict[key] = value.to_dict()
+        string = json.dumps(new_dict)
+        with open("file.json", "r") as f:
+            js = f.read()
+        self.assertEqual(json.loads(string), json.loads(js))
+
+    def test_base_pep8_conformance_file_storage(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['/models/engine/file_storage.py'])
+        self.assertEqual(result.total_errors, 1)
+
+    def test_base_pep8_conformance_filesto_test(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files([
+            '/tests/test_models/test_engine/test_file_storageconsole.py'])
+        self.assertEqual(result.total_errors, 1)
