1ea839ff1a5f31b504c1b8fdd339a557c978708c
julianfrancor <julianfrancor@gmail.com>
2020-07-01 00:31:43 +0000

    modifying init

--- models/__init__.py
+++ models/__init__.py
@@ -3,13 +3,7 @@
 [summary]
 """
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.state import State
-from models.review import Review
-from models.user import User
+
 
 
 storage = FileStorage()
